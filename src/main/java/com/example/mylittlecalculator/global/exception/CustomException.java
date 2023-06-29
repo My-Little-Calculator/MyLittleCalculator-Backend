3759ae39d4d890fb948bef8bacca8b789b352d2e
sejun An <asjjun@naver.com>
2023-06-29 19:47:38 +0900

    feat: 커스텀 예외처리 기능 구현
    - 전역적인 공통 로직 구현
    
    Closes #13

---
+++ src/main/java/com/example/mylittlecalculator/global/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.example.mylittlecalculator.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+}
